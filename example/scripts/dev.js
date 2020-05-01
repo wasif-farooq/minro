16f07eb6b5b7682f3fd476e62c9f721996ce3236
Wasif <wasif@192.168.10.3>
2020-05-01 17:46:43 +0500

    update dev config

--- example/scripts/dev.js
+++ example/scripts/dev.js
@@ -12,34 +12,38 @@ const config = webpackConfig(process.env.NODE_ENV || 'development');
 let thread = false;
 const compiler = webpack(config);
 
-compiler.watch({
-    aggregateTimeout: 1000
-}, (err, stats) => {
-
+compiler.hooks.watchRun.tap('stopProcess', (compiler) => {
     if (thread) {
         thread.stdin.pause();
         thread.kill();
     }
+});
 
-    process.on('unhandledRejection', err => {
+process.on('unhandledRejection', err => {
+    if (thread) {
         thread.stdin.pause();
         thread.kill();
-        throw err;
-    });
+    }
+    throw err;
+});
 
-    ['SIGINT', 'SIGTERM'].forEach(function(sig) {
-        process.on(sig, function() {
+['SIGINT', 'SIGTERM'].forEach(function(sig) {
+    process.on(sig, function() {
+        if (thread) {
             thread.stdin.pause();
             thread.kill();
-            process.exit();
-        });
+        }
+        process.exit();
     });
+});
 
-    setTimeout(() => {
-        thread = spawn('node', [config.output.path + '/index.js']);
-        thread.stdout.on('data', data => console.log(data.toString()));
-        thread.stdout.on('message', data => console.log(data.toString()));
-        thread.stderr.on('data', data => console.warn(data.toString()));
-    }, 500);
+compiler.watch({
+    aggregateTimeout: 1000
+}, (err, stats) => {
 
-})
+    thread = spawn('node', [config.output.path + '/index.js']);
+    thread.stdout.on('data', data => console.log(data.toString()));
+    thread.stdout.on('message', data => console.log(data.toString()));
+    thread.stderr.on('data', data => console.warn(data.toString()));
+
+});
--- example/src/index.js
+++ example/src/index.js
@@ -2,7 +2,7 @@ import http from 'http'
 
 const Server = () => {
     const server = http.createServer(function (req, res) {
-        console.log('get the request asd asasdasdsad show')
+        console.log('get the request asd asasdasdsad show asd')
         //console.log("NODE_APP_DATA_URL : ", process.env.NODE_APP_DATA_URL)
         res.end('hello world' + process.env.NODE_APP_DATA_URL)
     })
