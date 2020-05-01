e57ae450496ffd728b42f7d11905302232b3c322
Wasif <wasif@192.168.10.3>
2020-05-01 20:56:59 +0500

    setup profiling and add husky and pre hooks

--- example/lib/index.js
+++ example/lib/index.js
@@ -1,26 +1,2 @@
-"use strict";
-
-var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.default = void 0;
-
-var _http = _interopRequireDefault(require("http"));
-
-const Server = () => {
-  const server = _http.default.createServer(function (req, res) {
-    //console.log("NODE_APP_DATA_URL : ", process.env.NODE_APP_DATA_URL)
-    console.log('get the request asd asasdasdsad show asd'), res.end('hello world' + process.env.NODE_APP_DATA_URL);
-  });
-
-  server.listen(4002, () => {
-    console.log('Listening on port 4000');
-  });
-};
-
-Server();
-var _default = Server;
-exports.default = _default;
-module.exports = exports.default;
+"use strict";var _interopRequireDefault=require("@babel/runtime/helpers/interopRequireDefault");var _http=_interopRequireDefault(require("http"));Object.defineProperty(exports,"__esModule",{value:!0}),exports.default=void 0;const Server=()=>{const a=_http.default.createServer(function(a,b){//console.log("NODE_APP_DATA_URL : ", process.env.NODE_APP_DATA_URL)
+console.log("get the request asd asasdasdsad show asd"),b.end("hello world"+process.env.NODE_APP_DATA_URL)});a.listen(4002,()=>{console.log("Listening on port 4000")})};Server();var _default=Server;exports.default=_default,module.exports=exports.default;
--- example/lib/test.js
+++ example/lib/test.js
@@ -1,14 +1,1 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.default = void 0;
-
-const test = () => {
-  console.log('test called');
-};
-
-var _default = test;
-exports.default = _default;
-module.exports = exports.default;
+"use strict";Object.defineProperty(exports,"__esModule",{value:!0}),exports.default=void 0;const test=()=>{console.log("test called")};var _default=test;exports.default=_default,module.exports=exports.default;
